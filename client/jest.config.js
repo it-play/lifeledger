685cdb4abf099a90d922a8e57d733611ca45bee0
snowykte0426 <snowykte0426@naver.com>
2026-07-26 04:14:08 +0900

    chore(client): jest 설정 주석 제거

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,7 +1,3 @@
-/**
- * 단위 테스트만 돌린다 (AGENTS.md 의 테스트 정책 참고).
- * ESM 소스를 @swc/jest 로 변환하고, `./x.js` 형태의 확장자 있는 상대 경로를 .ts 로 되돌린다.
- */
 module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
